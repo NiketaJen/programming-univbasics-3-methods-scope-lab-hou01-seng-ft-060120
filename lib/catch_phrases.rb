7f1b645c86274ab190b2b274280b6ee6b56079e4
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-05 04:53:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 def mario
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase='"It's a-me, Mario!"'
+  phrase="""It's a-me, Mario!"'
   puts phrase
 end
 
 def toadstool
-  puts "#{$status}""
+  puts "#{$status}"
 end
 
 def link 
