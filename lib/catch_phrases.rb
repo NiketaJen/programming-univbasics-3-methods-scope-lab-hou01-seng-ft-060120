9376b698f845e2decf2643c2d2d4e634e925ce39
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-05 05:02:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 def mario
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase="'It's a-me, Mario!'"
-  puts phrase
+  puts "#{phrase}"
 end
 
 def toadstool
@@ -10,7 +10,7 @@ end
 
 def link 
   phrase= "'It's Dangerous To Go Alone! Take This.'"
-  puts phrase
+  puts "#{phrase}"
 end
 
 def all_phrases
